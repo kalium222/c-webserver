151b52eed6fa1a9d6c0eacb1a0f07c5cc2955273
kalium <kalium222@outlook.com>
2024-01-15 15:59:47 +0800

    feat: use ctest

--- main.c
+++
@@ -1,6 +0,0 @@
-#include "socket_helper.h"
-
-int main(int argc, char **argv) {
-	return 0;
-}
-
--- model.h
+++
@@ -1,8 +0,0 @@
-#pragma once // MODEL_H_
-
-#define MAXLINE 1024
-
-int client(char* host, char* port); 
-
-int server(char* port);
-

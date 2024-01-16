b4d2d728808b89b191044c0fd52bbac70969382b
kalium <kalium222@outlook.com>
2024-01-16 20:51:40 +0800

    test: model

--- test/test_model.c
+++ test/test_model.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
 
-#include "../src/socket_helper.h"
+#include "../src/model.h"
 
+#define PORT "11111"
+
+// how to test it?
 int main(void) {
+	pid_t pid;
+	pid = fork();
+	if (pid<0) {
+		fprintf(stderr, "Fork error\n");
+		return 1;
+	} else if (pid==0) {
+		// child
+		client("xps-13-9300", PORT);
+	} else {
+		// parent
+		server(PORT);
+	}
 	return 0;
 }
 

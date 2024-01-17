4357a532b77b2a496ff7cc00793eec0acd5b70b4
kalium <kalium222@outlook.com>
2024-01-17 22:12:05 +0800

    feat: parse_uri

--- src/http_handler.c
+++ src/http_handler.c
@@ -7,17 +7,21 @@
 #include <unistd.h>
 
 void doit(int fd) {
+	// Whether this request is static
 	int is_static;
-	/*struct stat sbuf;*/
+	// lots of buffers
 	char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
 	char filename[MAXLINE], cgiargs[MAXLINE];
+	// file state of filename
+	struct stat sbuf;
 
 	fscanf(fdopen(fd, "r"), "%s %s %s", method, uri, version);
 	printf("Request headers:\n");
 	printf("%s %s %s", method, uri, version);
 	
 	if (strcasecmp(method, "GET")) {
-		// TODO: error
+		clienterror(fd, method, "501", "Not Implemented", 
+				"Tiny does not implement this method");
 		return;
 	}
 
@@ -43,5 +47,30 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
 	write(fd, buf, strlen(buf));
 	write(fd, body, strlen(body));
 	return;
+}
+
+int parse_uri(char *uri, char *filename, char *cgiargs) {
+	if (!strstr(uri, "cgi-bin")) {
+		// static
+		strcpy(cgiargs, "");
+		strcpy(filename, ".");
+		strcat(filename, uri);
+		if (uri[strlen(uri)-1]=='/') {
+			strcat(filename, "home.html");
+		}
+		return 1;
+	} else {
+		// dynamic
+		char* p=index(uri, '?');
+		if (p) {
+			strcpy(cgiargs, p+1);
+			*p = '\0';
+		} else {
+			strcpy(cgiargs, "");
+		}
+		strcpy(filename, ".");
+		strcpy(filename, uri);
+		return 0;
+	}
 }
 
--- src/http_handler.h
+++ src/http_handler.h
@@ -2,11 +2,15 @@
 
 #define ERROR_TITLE "<html><title>Tiny Error</title>"
 
+// handle the http request on fd socket
 void doit(int fd);
 
+// write error to fd socket according to the 
+// following things
 void clienterror(int fd, char *cause, char* errnum, char* shortmsg, char* longmsg);
 
 void read_requesthdrs();
 
+// return whether the request is static
 int parse_uri(char* uri, char* filename, char* cgiargs);
 
